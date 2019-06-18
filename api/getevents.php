f2337f38f3e31c10dbdd616c8ebf22ec1888c15a
sajalsuraj <sajal.suraj@gmail.com>
2019-06-19 00:15:34 +0530

    credentials removed

--- api/getevents.php
+++ api/getevents.php
@@ -2,7 +2,7 @@
     $ch = curl_init();
     curl_setopt_array(
         $ch, array(
-        CURLOPT_URL => 'https://www.eventbriteapi.com/v3/events/search/?sort_by=date&user.id=2485622408&token=EBBXN7NEAFOFVUQFYP3Y',
+        CURLOPT_URL => 'https://www.eventbriteapi.com/v3/events/search/?sort_by=date&user.id=userid&token=token',
         CURLOPT_RETURNTRANSFER => true
     ));
         
